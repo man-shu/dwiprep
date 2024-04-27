64309fc716abc885b29dadfc37c6d922bc986daf
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-04-27 21:03:37 +0200

    fix docker run script

--- run_dmriprep.sh
+++ run_dmriprep.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [[ $1==local ]]; then
+if [[ $1 == "local" ]]; then
     ROOT='/Users/himanshu/Desktop/diffusion'
 else
     ROOT='/storage/store3/work/haggarwa/diffusion'
 fi
 BIDS_DATA=$ROOT'/bids_data'
 OUTPUT_DIR=$ROOT'/result'
+echo "docker run -it --rm -v $BIDS_DATA:/data:ro -v $OUTPUT_DIR:/out $2 /data /out/out participant --verbose --fs-no-reconall"
 docker run -it --rm -v $BIDS_DATA:/data:ro -v $OUTPUT_DIR:/out $2 /data /out/out participant --verbose --fs-no-reconall
