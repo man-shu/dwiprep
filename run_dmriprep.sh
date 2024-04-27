dd0bbfec7377ac002f3f9888480c03ae4a53b5fd
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-04-27 09:37:12 +0200

    better run bash script

--- run_dmriprep.sh
+++ run_dmriprep.sh
@@ -1,1 +1,9 @@
-docker run -it --rm -v /storage/store3/work/haggarwa/diffusion/bids_data:/data:ro -v result:/out 251cb87b94f5 /data /out/out participant --verbose --fs-no-reconall
+#!/bin/bash
+if $1 =="local" then
+    $ROOT=/Users/himanshu/Desktop/diffusion
+else
+    $ROOT=/storage/store3/work/haggarwa/diffusion
+fi
+$BIDS_DATA=$ROOT/bids_data
+$OUTPUT_DIR=$ROOT/result
+docker run -it --rm -v $BIDS_DATA:/data:ro -v $OUTPUT_DIR:/out $2 /data /out/out participant --verbose --fs-no-reconall
